056d9f0e5d98a92ed42d941eab1ed709bed5fd10
Mostafa Sayed <mostafa-m-sayed@hotmail.com>
2024-10-28 16:34:31 +0300

    Initial Commit

---
+++ Wishory/WishoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishoryApp.swift
+//  Wishory
+//
+//  Created by Mostafa Sayed on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WishoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WishoryTests/WishoryTests.swift
@@ -0,0 +1,16 @@
+//
+//  WishoryTests.swift
+//  WishoryTests
+//
+//  Created by Mostafa Sayed on 28/10/2024.
+//
+
+import Testing
+
+struct WishoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
