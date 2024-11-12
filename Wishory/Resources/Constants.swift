b4399e58b4899bb143b435b29fb6b56a00a4dbff
Mostafa Sayed <mostafa-m-sayed@hotmail.com>
2024-11-12 13:41:40 +0200

    Adding App Colors

--- Wishory/Resources/Constants.swift
+++ Wishory/Resources/Constants.swift
@@ -5,8 +5,13 @@
 //  Created by Mostafa Sayed on 03/11/2024.
 //
 
+import SwiftUI
 
 class Constants {
     static let randomPhotoURL = "https://picsum.photos/200/300"
     static let mocking = true
+    
+    enum AppColors {
+        static let theme = Color.darkGreen
+    }
 }
